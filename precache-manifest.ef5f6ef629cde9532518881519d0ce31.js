01e17dab3e152feb1da2cc48b706c83748a3ce68
Robynn Ding <hbkxmqdjn@gmail.com>
2020-08-04 19:31:28 -0400

    Updates

---
+++ precache-manifest.ef5f6ef629cde9532518881519d0ce31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b274fd91e640967f0d1d081bdc8dd5fe",
+    "url": "/drag-n-drop-column/index.html"
+  },
+  {
+    "revision": "c316419cf4f28e6feed6",
+    "url": "/drag-n-drop-column/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a2f8a9075e95dcfb0619",
+    "url": "/drag-n-drop-column/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c316419cf4f28e6feed6",
+    "url": "/drag-n-drop-column/static/js/2.90e05856.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/drag-n-drop-column/static/js/2.90e05856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f8a9075e95dcfb0619",
+    "url": "/drag-n-drop-column/static/js/main.92a05f8b.chunk.js"
+  },
+  {
+    "revision": "1f7a87a66db034eedb5f",
+    "url": "/drag-n-drop-column/static/js/runtime-main.74ad019b.js"
+  }
+]);
